482fa91e67f94876c510e6b0efaa2e78f7602afb
499602D2 <46869890+499602D2@users.noreply.github.com>
2021-01-14 22:41:56 +0200

    Update resizeBot.py

--- resizeBot.py
+++ resizeBot.py
@@ -337,11 +337,6 @@ if __name__ == '__main__':
 			Filters.document.category("image") & ~Filters.photo,
 			callback=document_to_bytearray))
 
-	dispatcher.add_handler(
-		MessageHandler(
-			Filters.document.file_extension("webp"),
-			callback=document_to_bytearray))
-
 	# handle commands
 	dispatcher.add_handler(CommandHandler(command=('start'), callback=start))
 	dispatcher.add_handler(CommandHandler(command=('source'), callback=source))
