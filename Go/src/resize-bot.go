1f0693bf3b2e65d39027e94a0447cf5083917400
499602D2 <46869890+499602D2@users.noreply.github.com>
2021-05-16 18:58:52 +0300

    Keep track of unique chats in json
    A very simple way of keeping track of unique chat count. This is combined with a simple binary search of the sorted []int slice, which should be a lot faster than a dumb linear search.
    
    The stat is updated in a goroutine, after every image the bot converts.

--- Go/src/resize-bot.go
+++ Go/src/resize-bot.go
@@ -10,6 +10,7 @@ import (
 	"time"
 	"bytes"
 	"io"
+	"sort"
 
 	"os"
 	"bufio"
@@ -24,10 +25,10 @@ import (
 )
 
 func resizeImage(imgBytes []byte) ([]byte, string, error) {
-	// build image from buffer
+	// Build image from buffer
 	img, err := vips.NewImageFromBuffer(imgBytes)
 	if err != nil {
-		fmt.Println("Error decoding image! Err: ", err)
+		log.Println("Error decoding image! Err: ", err)
 
 		errorMsg := fmt.Sprintf("⚠️ Error decoding image: %s.", err.Error())
 		if err.Error() == "unsupported image format" {
@@ -37,10 +38,10 @@ func resizeImage(imgBytes []byte) ([]byte, string, error) {
 		return nil, errorMsg, err
 	}
 
-	// w, h for resize (int)
+	// Dimensions for resize (int)
 	w, h := img.Width(), img.Height()
 
-	// determine the factor by how much to scale the image with (vips wants int64)
+	// Determine the factor by how much to scale the image with (vips wants f64)
 	var resScale float64
 
 	if w >= h {
@@ -49,16 +50,16 @@ func resizeImage(imgBytes []byte) ([]byte, string, error) {
 		resScale = 512.0 / float64(h)
 	}
 
-	// resize, upscale status
+	// Resize, upscale status
 	err = img.Resize(resScale, -1)
 	imgUpscaled := resScale > 1
 
-	// construct params for png export
+	// Construct params for png export
 	var pngBuff []byte
 	compressionFailed := false
 	compression := 0
 
-	// compress image
+	// Increment compression ratio if size is too large
 	for {
 		pngParams := vips.PngExportParams{
 			StripMetadata:	true,
@@ -72,43 +73,43 @@ func resizeImage(imgBytes []byte) ([]byte, string, error) {
 			log.Fatal("Error encoding image as png: ", err)
 			if err.Error() == "unsupported image format" {
 				return nil, "⚠️ Unsupported image format!", err
+			} else {
+				return nil, fmt.Sprintf("⚠️ Error: %s", err.Error()), err
 			}
 		}
 
 		// check filesize is within limits (max. 512 KB)
 		if len(pngBuff) / 1024 >= 512 {
 			if compression < 10 {
-				compression++; continue;
+				compression++; continue
 			} else {
-				compressionFailed = true
-				break
+				compressionFailed = true; break
 			}
 		} else {
 			break
 		}
 	}
 
-	// construct the caption
+	// Construct the caption
 	imgCaption := fmt.Sprintf(
 		"🖼 Here's your sticker-ready image (%dx%d)! Forward this to @Stickers.",
 		img.Width(), img.Height(),
 	)
 
-	// add notice to user if image was upscaled or compressed
+	// Add notice to user if image was upscaled or compressed
 	if imgUpscaled == true {
 		imgCaption += "\n\n⚠️ Image upscaled! Quality may have been lost: consider using a larger image."
 	} else if compressionFailed == true {
 		imgCaption += "\n\n⚠️ Image compression failed (≥512 KB): you must manually compress the image!"
 	}
 
-	// adios
 	img.Close()
 	return pngBuff, imgCaption, nil
 }
 
 func getBytes(bot *tb.Bot, message *tb.Message, mediaType string) []byte {
-	// get file
-	var err error; var file io.ReadCloser;
+	// Get file from tg servers
+	var err error; var file io.ReadCloser
 	if mediaType == "photo" {
 		file, err = bot.GetFile(&message.Photo.File)
 	} else {
@@ -119,7 +120,7 @@ func getBytes(bot *tb.Bot, message *tb.Message, mediaType string) []byte {
 		log.Fatal("Error running GetFile: ", err)
 	}
 
-	// download to buffer
+	// Download (copy) to buffer
 	var imgBuf bytes.Buffer
 	_, err = io.Copy(&imgBuf, file)
 
@@ -127,7 +128,7 @@ func getBytes(bot *tb.Bot, message *tb.Message, mediaType string) []byte {
 }
 
 func sendDocument(bot *tb.Bot, message *tb.Message, photo []byte, imgCaption string) {
-	// send as a document: create object
+	// Send as a document: create object
 	doc := tb.Document{
 		File: 		tb.FromReader(bytes.NewReader(photo)),
 		Caption: 	imgCaption,
@@ -135,21 +136,51 @@ func sendDocument(bot *tb.Bot, message *tb.Message, photo []byte, imgCaption str
 		FileName: 	fmt.Sprintf("resized-image-%d.png", time.Now().Unix()),
 	}
 
-	// disable notifications
+	// Disable notifications
 	sendOpts := tb.SendOptions{ DisableNotification: true, }
 
-	// send
+	// Send
 	_, err := doc.Send(bot, message.Sender, &sendOpts)
 
 	if err != nil {
-		fmt.Println("Error sending message:", err)
+		log.Println("Error sending message:", err)
 	}
+}
+
+func updateUniqueStat(uid *int, config *Config) {
+	// uarr is always sorted when performing check
+	i := sort.Search(
+		len(config.UniqueUsers),
+		func(i int) bool { return config.UniqueUsers[i] >= *uid },
+	)
+
+	if i < len(config.UniqueUsers) && config.UniqueUsers[i] == *uid {
+		return // uid exists in the array
+	} else {
+		if len(config.UniqueUsers) == i {
+			// nil or empty slice, or after last element
+			config.UniqueUsers = append(config.UniqueUsers, *uid)
+		} else if i == 0 {
+			// if zeroth index, append
+			config.UniqueUsers = append([]int{ *uid }, config.UniqueUsers...)
+		} else {
+			// otherwise, we're inserting in the middle of the array
+			config.UniqueUsers = append(config.UniqueUsers[:i+1], config.UniqueUsers[i:]...)
+			config.UniqueUsers[i] = *uid
+		}
+	}
+
+	// Stat++
+	config.StatUniqueChats++
+	return
 }
 
 type Config struct {
 	Token 			string
-	StatConverted 	uint32
+	StatConverted 	int
+	StatUniqueChats	int
 	StatStarted		int64
+	UniqueUsers		[]int
 }
 
 func dumpConfig(config *Config) {
@@ -163,112 +194,178 @@ func dumpConfig(config *Config) {
 		log.Fatal(err); os.Exit(1)
 	}
 
-	// write, close
+	// Write, close
 	file.Write(jsonbytes); file.Close()
 }
 
-func loadToken() Config {
-	// config doesn't exist: create
+func loadConfig() Config {
+	// Config doesn't exist: create
 	if _, err := os.Stat("botConfig.json"); os.IsNotExist(err) {
 		fmt.Print("\nEnter bot token: ")
+
 		reader := bufio.NewReader(os.Stdin)
 		inp, _ := reader.ReadString('\n')
 		botToken := strings.TrimSuffix(inp, "\n")
 
-		// marshal
-		config := Config { Token: botToken, StatConverted: 0, StatStarted: time.Now().Unix() }
+		// Create, marshal
+		config := Config {
+			Token: botToken,
+			StatConverted: 0,
+			StatUniqueChats: 0,
+			StatStarted: time.Now().Unix(),
+			UniqueUsers: []int{},
+		}
+
 		dumpConfig(&config)
 		return config
 	}
 
-	// config exists: load
+	// Config exists: load
 	fbytes, err := ioutil.ReadFile("botConfig.json")
 	if err != nil {
-		log.Fatalf("Error reading config file: %s", err)
+		log.Println("Error reading config file: %s", err)
 		os.Exit(1)
 	}
 
-	// new config struct
+	// New config struct
 	var config Config
 
-	// unmarshal into our config struct
-	json.Unmarshal(fbytes, &config)
+	// Unmarshal into our config struct
+	err = json.Unmarshal(fbytes, &config)
+	if err != nil {
+		log.Fatal("Error unmarshaling config json: ", err)
+		os.Exit(1)
+	}
 
-	// set startup time
+	// Set startup time
 	config.StatStarted = time.Now().Unix()
+	config.StatUniqueChats = len(config.UniqueUsers)
+
+	// Sort UniqueChats, as they may be unsorted
+	sort.Ints(config.UniqueUsers)
 
 	return config
 }
 
 func main() {
-	// version 0.1: 2020.3.29
-	fmt.Println("go-resize-bot 2020.5.15")
+	const vnum string = "2020.5.16"
 
-	config := loadToken()
+	// Set-up logging
+	logf, err := os.OpenFile("log-file.log", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Defer to close when you're done with it, not because you think it's idiomatic!
+	defer logf.Close()
+
+	// Set output of logs to f
+	log.SetOutput(logf)
+
+	// Version 0.1: 2020.3.29
+	log.Println("go-resize-bot", vnum)
+	log.Println("Bot started at", time.Now())
+
+	config := loadConfig()
 	bot, err := tb.NewBot(tb.Settings{
 		Token: config.Token,
 		Poller: &tb.LongPoller{ Timeout: 10 * time.Second },
 	})
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error starting bot: ", err)
 		return
 	}
 
 	// https://pkg.go.dev/github.com/davidbyttow/govips/v2@v2.6.0/vips#LoggingSettings
 	vips.LoggingSettings(nil, vips.LogLevel(3))
 
-	// command handler for /stats
+	// Command handler for /start
+	bot.Handle("/start", func(message *tb.Message) {
+		startMessage := "🖼 Hi there! To use the bot, simply send an image to this chat (jpg/png)."
+		bot.Send(message.Sender, startMessage)
+		log.Println("🌟", message.Sender.ID, "bot added to new chat!")
+	})
+
+	// Command handler for /help
+	bot.Handle("/help", func(message *tb.Message) {
+		helpMessage := "🖼 To use the bot, simply send your image to this chat (jpg/png)!"
+		bot.Send(message.Sender, helpMessage)
+		log.Println("🙋‍♂️", message.Sender.ID, "requested help!")
+	})
+
+	// Keep track of the last chat to convert an image;
+	// this should reduce updateUniqueStat checks a lot
+	var lastUser int
+
+	// Command handler for /stats
 	bot.Handle("/stats", func(message *tb.Message) {
 		msg := fmt.Sprintf(
-			"📊 *Bot statistics*\nImages converted: %d\nBot started %s",
+			"📊 *Bot statistics*\nImages converted: %d\nUnique users seen: %d",
 			config.StatConverted,
+			config.StatUniqueChats,
+		)
+
+		msg += fmt.Sprintf("\n\n*🎛 Server information*\nBot started %s",
 			humanize.RelTime(time.Unix(config.StatStarted, 0), time.Now(), "ago", "ago"),
 		)
 
-		// add Markdown parsing for a pretty link embed
-		sopts := tb.SendOptions{ ParseMode: "Markdown", }
+		// Add Markdown parsing for a pretty link embed
+		sopts := tb.SendOptions{ ParseMode: "Markdown" }
 
-		// add link to github
-		msg += "\n\n🐙 Source code is on [Github!](https://github.com/499602D2/tg-resize-sticker-images)"
+		// Add vnum, link
+		msg += fmt.Sprintf(
+			"\nRunning version [%s](https://github.com/499602D2/tg-resize-sticker-images)",
+			vnum,
+		)
+
 		bot.Send(message.Sender, msg, &sopts)
+		log.Println("📊", message.Sender.ID, "requested to view stats!")
 	})
 
-	// command handler for /stats
-	bot.Handle("/help", func(message *tb.Message) {
-		helpMessage := "🖼 To use the bot, simply send your image to this chat (jpg/png)."
-		bot.Send(message.Sender, helpMessage)
-	})
-
-	// register photo handler
+	// Register photo handler
 	bot.Handle(tb.OnPhoto, func(message *tb.Message) {
-		// resize photo
+		// Resize photo
 		photo, imgCaption, err := resizeImage(getBytes(bot, message, "photo"))
 
-		// send
+		// Send
 		if err != nil {
 			bot.Send(message.Sender, imgCaption)
 		} else {
 			sendDocument(bot, message, photo, imgCaption)
 			config.StatConverted += 1
+			log.Println("🖼", message.Sender.ID, "successfully converted an image!")
+		}
+
+		// Update stat for count of unique chats in a goroutine
+		if message.Sender.ID != lastUser {
+			go updateUniqueStat(&message.Sender.ID, &config)
+			lastUser = message.Sender.ID
 		}
 	})
 
-	// register document handler
+	// Register document handler
 	bot.Handle(tb.OnDocument, func(message *tb.Message) {
-		// resize photo
+		// Resize photo
 		photo, imgCaption, err := resizeImage(getBytes(bot, message, "document"))
 
-		// send
+		// Send
 		if err != nil {
 			bot.Send(message.Sender, imgCaption)
 		} else {
 			sendDocument(bot, message, photo, imgCaption)
 			config.StatConverted += 1
+			log.Println("🖼", message.Sender.ID, "successfully converted an image!")
+		}
+
+		// Update stat for count of unique chats in a goroutine
+		if message.Sender.ID != lastUser {
+			go updateUniqueStat(&message.Sender.ID, &config)
+			lastUser = message.Sender.ID
 		}
 	})
 
-	// dump statistics to disk once every 30 minutes
+	// Dump statistics to disk once every 30 minutes
 	scheduler := gocron.NewScheduler(time.UTC)
 	scheduler.Every(30).Minutes().Do(dumpConfig, &config)
 	scheduler.StartAsync()
